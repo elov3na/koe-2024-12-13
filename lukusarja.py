7a37aea774870c6eed6d0ca98d055b97cd6a7e6d
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2024-12-11 12:17:29 +0000

    Lukusarja-tehtävänannon selkeytys

--- lukusarja.py
+++ lukusarja.py
@@ -4,37 +4,41 @@ Lukusarja
 Tehtäväsi on toteuttaa funktio `lukusarja`, joka saa parametrinaan positiivisen kokonaisluvun ja
 muodostaa sekä palauttaa listan kokonaislukuja alla kuvailtujen sääntöjen mukaisesti.
 
-Lista alkaa aina funktiolle annetusta luvusta ja se päättyy ykköseen. Annetusta luvusta lähtien
-listan seuraava luku määräytyy aina seuraavien sääntöjen mukaan:
+Muodostettavan listan täytyy alkaa funktiolle annetusta luvusta ja sen täytyy päättyä ykköseen.
+Ensimmäisen luvun jälkeen seuraava luku asetetaan aina seuraavien sääntöjen mukaan:
 
-1. jos edellinen luku on parillinen, se jaetaan kahdella
-2. jos edellinen luku on pariton, se kerrotaan kolmella ja tuloon lisätään yksi.
+1. jos edellinen luku on parillinen, se jaetaan kahdella ja tulos lisätään listalle
+2. jos edellinen luku on pariton, se kerrotaan kolmella ja tuloon lisätään yksi, ja tulos lisätään listalle.
 
-Jos siis lukusarjan ensimmäinen luku on esimerkiksi seitsemän, seuraava luku saadaan kertomalla
-7 kolmella ja lisäämällä yksi: 7 * 3 + 1 = 22. Jos lukusarjan edellinen luku on puolestaan 22,
-saadaan seuraava luku jakamalla tämä luku kahdella 22 / 2 = 11.
+Tätä logiikkaa toistetaan, kunnes saavutetaan luku 1.
 
-Tätä logiikkaa jatketaan, kunnes lukusarja saavuttaa ykkösen. Lopuksi kaikki käsitellyt luvut
-palautetaan listana.
+Jos siis lukusarjan ensimmäinen luku on esimerkiksi seitsemän (pariton), seuraava luku saadaan kertomalla
+7 kolmella ja lisäämällä yksi: 7 * 3 + 1 = 22. Luvusta 22 (parillinen) seuraava luku saadaan jakamalla
+se kahdella, eli 22 / 2 = 11. Tätä logiikkaa jatketaan, kunnes lukusarja saavuttaa ykkösen:
 
-Seuraavassa esimerkissä aloitetaan luvusta kahdeksan ja kaikki sarjan luvut ykköseen asti sattuvat
-olemaan parillisia. Seuraava luku saaadan siis tässä aina jakamalla edellinen kahdella:
+7 -> 22 -> 11 -> 34 -> 17 -> 52 -> 26 -> 13 -> 40 -> 20 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
 
-    >>> lukusarja(8)
-    [8, 4, 2, 1]
-
-Seuraavissa esimerkeissä on myös parittomia lukuja, joiden tapauksessa seuraava luku saadaan kertomalla
-luku kolmella ja lisäämällä tuloon yksi:
-
-    >>> lukusarja(3)
-    [3, 10, 5, 16, 8, 4, 2, 1]
+Lopuksi kaikki käsitellyt luvut palautetaan listana:
 
     >>> lukusarja(7)
     [7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
 
+
+Seuraavassa lyhyemmässä esimerkissä aloitetaan luvusta kahdeksan ja kaikki sarjan luvut ykköseen asti
+sattuvat olemaan parillisia. Seuraava luku saaadan siis tässä aina jakamalla edellinen kahdella:
+
+    >>> lukusarja(8)
+    [8, 4, 2, 1]
+
+Seuraavassa esimerkissä on myös parittomia lukuja, joiden tapauksessa seuraava luku saadaan kertomalla
+edellinen kolmella ja lisäämällä tuloon yksi:
+
+    >>> lukusarja(3)
+    [3, 10, 5, 16, 8, 4, 2, 1]
+
 Huomaa, että funktio ei saa tulostaa tulosta, vaan lista täytyy palauttaa paluuarvona:
 
-    >>> lukusarja(3) == [3, 10, 5, 16, 8, 4, 2, 1]
+    >>> lukusarja(2) == [2, 1]      # älä tulosta, vaan palauta!
     True
 
 
